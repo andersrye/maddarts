3f1b1cf7c5d893df89786d5d6c87d2bfebed505a
Anders S Rye <ary@teleplan.no>
2014-04-23 06:45:37 +0000

    restructure

--- pi-dart/boardmon.py
+++ pi-dart/boardmon.py
@@ -2,9 +2,19 @@ import serial, time, sys, subprocess
 
 p = subprocess.Popen(["socat", "-", "UDP-DATAGRAM:255.255.255.255:5000,broadcast"], stdin=subprocess.PIPE)
 
+def set_procname(newname):
+    from ctypes import cdll, byref, create_string_buffer
+    libc = cdll.LoadLibrary('libc.so.6')    #Loading a 3rd party library C
+    buff = create_string_buffer(len(newname)+1) #Note: One larger than the name (man prctl says that)
+    buff.value = newname                 #Null terminated string as it should be
+    libc.prctl(15, byref(buff), 0, 0, 0) #Refer to "#define" of "/usr/include/linux/prctl.h" for the misterious value 16 & arg[3..5] are zero as the man page says.
+
+
 if(len(sys.argv) < 2):
 	print "Missing board ID"
 	sys.exit(0)
+
+set_procname("boardmon")
 
 s = serial.Serial(
 	port='/dev/ttyUSB0',
